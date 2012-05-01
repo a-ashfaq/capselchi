ef2d86a373b56b5f3f9258da8bc21db77ec8e754
stefan.diels <stefan.diels@gmail.com>
2012-05-01 18:35:37 +0000

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 1-mei-2012
+ *      Author: stefan
+ */
+#include <assert.h>
+#include <stddef.h>
+
+namespace capselchi {
+
+int main(int argc, char **argv) {
+
+}
+
+}  // namespace capselchi
