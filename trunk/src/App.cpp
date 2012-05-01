e5bd2972d671b3e07f657395ad7e87141d234d0f
stefan.diels@gmail.com <stefan.diels@gmail.com@fbbe2d4b-5e7a-d0b1-097d-78337654c1c6>
2012-05-01 18:35:37 +0000

    initial commit

---
+++ trunk/src/App.cpp
@@ -0,0 +1,18 @@
+/*
+ * App.cpp
+ *
+ *  Created on: 1-mei-2012
+ *      Author: stefan
+ */
+
+#include <assert.h>
+#include <stddef.h>
+
+namespace capselchi {
+
+
+
+}  // namespace capselchi
+
+
+
