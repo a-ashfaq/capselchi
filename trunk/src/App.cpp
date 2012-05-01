74571164fe632e2f61392fa586320085e5dab70c
stefan.diels@gmail.com <stefan.diels@gmail.com@fbbe2d4b-5e7a-d0b1-097d-78337654c1c6>
2012-05-01 20:27:45 +0000

    skeleton code for game loop and input listeners

--- trunk/src/App.cpp
+++ trunk/src/App.cpp
@@ -7,10 +7,26 @@
 
 #include <assert.h>
 #include <stddef.h>
+#include "App.hpp"
 
 namespace capselchi {
 
+	int App::run(void){
+		//TODO game loop
+		return 0;
+	}
 
+	void App::init(void){
+		//TODO game initialization
+	}
+
+	void App::loop(void){
+		//TODO game loop
+	}
+
+	void App::cleanup(void){
+		//TODO game cleanup
+	}
 
 }  // namespace capselchi
 
---
+++ trunk/src/Balloon.cpp
@@ -0,0 +1,16 @@
+/*
+ * Balloon.cpp
+ *
+ *  Created on: 1-mei-2012
+ *      Author: stefan
+ */
+#include <stddef.h>
+#include <assert.h>
+#include "Balloon.hpp"
+namespace capselchi {
+
+
+
+}  // namespace capselchi
+
+
