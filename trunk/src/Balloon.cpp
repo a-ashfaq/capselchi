8aef752077a9aa03e23f0c52d8d5bb824e839971
stefan.diels@gmail.com <stefan.diels@gmail.com@fbbe2d4b-5e7a-d0b1-097d-78337654c1c6>
2012-05-29 15:52:38 +0000

    engine framework

--- trunk/src/Balloon.cpp
+++ trunk/src/Balloon.cpp
@@ -9,7 +9,42 @@
 #include "Balloon.hpp"
 namespace capselchi {
 
+	Balloon::Balloon(World* world, string colorName, float32 x, float32 y, float32 r, b2BodyDef bodyDef){
+		position = new b2Vec2(x,y);
+		this->r = r;
+		this->colorName = colorName;
+		bodyDef.userData = this;
+		bodyDef.type = b2_dynamicBody;
+		this->world = world;
+		this->body = world->createBody(bodyDef);
+		this->world->createBall(this);
+	}
 
+	Balloon::~Balloon(){
+		body->SetUserData(NULL);
+		world->removeBall(this);
+		world->destroyBody(body);
+	}
+
+	b2Vec2 Balloon::getPösition(){
+		return position;
+	}
+
+	float32 Balloon::getRadius(){
+		return r;
+	}
+
+	b2Body* Balloon::getBody(){
+		return body;
+	}
+
+	string Balloon::getColor(){
+		return colorName;
+	}
+
+	int Balloon::isColor(string colorName){
+		return this->colorName.compare(colorName);
+	}
 
 }  // namespace capselchi
 
